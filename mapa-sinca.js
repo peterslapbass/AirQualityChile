0b070eed3620677517b7785ce3fa2d2de2a40122
peterslapbass <103006028+peterslapbass@users.noreply.github.com>
2026-04-17 13:50:13 -0400

    Update mapa-sinca.js

--- mapa-sinca.js
+++ mapa-sinca.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-
+  alert("MAPA-SINCA ACTIVO");
   console.log("✅ SCRIPT CARGADO - DOM READY");
 
   const map = L.map('map').setView([-33.45, -70.66], 5);
